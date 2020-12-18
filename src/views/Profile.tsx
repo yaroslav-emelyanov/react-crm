2227646b81b569c3969d415ccd0eaeafbf94cea0
Yaroslav Emelyanov <emelyanov.yaroslav.97@gmail.com>
2020-12-18 23:20:01 +0300

    Added functional changing name

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,23 +1,63 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useForm, SubmitHandler, FormProvider } from 'react-hook-form'
+import TextField from '../components/form/TextField'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from '../store/rootState'
+import { action } from '../store/rootActions'
+import { notification } from '../utils/plugins'
+
+interface DefaultValues {
+  name: string
+}
+
+const defaultValues: DefaultValues = {
+  name: '',
+}
 
 const Profile = () => {
+  const { name, bill } = useSelector((state: RootState) => state.info)
+  const { reset, ...formHook } = useForm({ defaultValues })
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    reset({ name })
+    setTimeout(() => M.updateTextFields())
+  }, [name, reset])
+
+  const onSubmit: SubmitHandler<DefaultValues> = async ({ name }) => {
+    await dispatch(action.updateUserInfo({ name, bill }))
+    notification.info('Имя пользователя обновлено')
+  }
+
   return (
     <div>
       <div className="page-title">
         <h3>Профиль</h3>
       </div>
 
-      <form className="form">
-        <div className="input-field">
-          <input id="description" type="text" />
-          <label htmlFor="description">Имя</label>
-          <span className="helper-text invalid">name</span>
-        </div>
+      <form className="form" onSubmit={formHook.handleSubmit(onSubmit)}>
+        <FormProvider reset={reset} {...formHook}>
+          <TextField
+            name="name"
+            label={'Имя'}
+            validationRules={{
+              required: { value: true, message: 'Вам нужно ввести своё имя' },
+            }}
+          />
+          <div className="switch" style={{ marginBottom: 16 }}>
+            <label>
+              English
+              <input type="checkbox" />
+              <span className="lever" />
+              Русский
+            </label>
+          </div>
 
-        <button className="btn waves-effect waves-light" type="submit">
-          Обновить
-          <i className="material-icons right">send</i>
-        </button>
+          <button className="btn waves-effect waves-light" type="submit">
+            Обновить
+            <i className="material-icons right">send</i>
+          </button>
+        </FormProvider>
       </form>
     </div>
   )
